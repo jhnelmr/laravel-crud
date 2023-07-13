3f672803bdd5fd19cc256976e9ca979d439e22b8
JEDRe <jhonelmar18u@gmail.com>
2023-07-13 17:02:30 +0800

    First commit: Initial project setup

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,23 @@
+
+ <!DOCTYPE html>
+
+<html>
+    <head>
+        <title>App Name </title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
+    </head>
+
+
+
+    <body>
+<div class="text-center">
+     <h1>Laravel 10 Crud Application</h1>
+</div>
+       
+        <div class="container">
+            <br>
+        
+            @yield('content')
+        </div>
+    </body>
+</html>
